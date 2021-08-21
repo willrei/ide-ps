6e4792c81490bda06f575f2a81c963ff0ce7f8c2
Igor De Latorre <delatorreigor0@gmail.com>
2021-08-21 13:14:15 -0300

    Added .txt file with the meal plans. Now the menu items changes by clicking on them

--- etapa3/index.js
+++ etapa3/index.js
@@ -1,7 +1,6 @@
 let pizzaSel = document.getElementById('pizza-selector');
 pizzaSel.addEventListener('click', addPizza);
 
-
 let saladSel = document.getElementById('salad-selector');
 saladSel.addEventListener('click', addSalad);
 
@@ -15,38 +14,158 @@ function addPizza() {
 
     let menu = document.getElementById('food-items');
 
-    let item = document.createElement('div');
-    item.setAttribute('class', 'food-item');
+    while(menu.firstChild){
+        menu.removeChild(menu.lastChild);
+    }
 
-    let itemName = document.createElement('div');
-    itemName.style.fontWeight = 'bold';
-    itemName.textContent = 'BBQ Chicken';
+    var file = "pizza.txt";
 
-    let itemPrice = document.createElement('div');
-    itemPrice.setAttribute('class', 'food-price');
-    itemPrice.textContent = '$26.90';
+    fetch(file)
+    .then(response => response.text())
+    .then(data =>{
+        var prato = data.split("\n");
+        var max = prato.length;
 
-    let itemDesc = document.createElement('div');
-    itemDesc.textContent = 'Hot chicken, barbecue sauce, ranch dressing, cheese';
+        for(var i = 0; i <  max; i++){
 
-    item.appendChild(itemName);
-    item.appendChild(itemPrice);
-    item.appendChild(itemDesc);
-    menu.appendChild(document.createElement('hr'));
-    menu.appendChild(item);
-    console.log(menu);
-}
+            let item = document.createElement('div');
+            item.setAttribute('class', 'food-item');
 
-function addStarter(){
-    starterButtonActivated();
+            let itemName = document.createElement('div');
+            itemName.style.fontWeight = 'bold';
+            itemName.textContent = prato[i];
+            i++;
+
+            let itemPrice = document.createElement('div');
+            itemPrice.setAttribute('class', 'food-price');
+            itemPrice.textContent = prato[i];
+            i++;
+
+            let itemDesc = document.createElement('div');
+            itemDesc.textContent = prato[i];
+            
+
+            menu.appendChild(item);
+            item.appendChild(itemName);
+            item.appendChild(itemPrice);
+            item.appendChild(itemDesc);
+            if(i < (max-1)){
+                menu.appendChild(document.createElement('hr'));
+            }
+        }
+    })
 }
 
 function addSalad(){
     saladButtonActivated();
 
+    let menu = document.getElementById('food-items');
+
+    while(menu.firstChild){
+        menu.removeChild(menu.lastChild);
+    }
+
+    var file = "salad.txt";
+
+    fetch(file)
+    .then(response => response.text())
+    .then(data =>{
+        var prato = data.split("\n");
+        var max = prato.length;
+
+        for(var i = 0; i <  max; i++){
+
+            let item = document.createElement('div');
+            item.setAttribute('class', 'food-item');
+
+            let itemName = document.createElement('div');
+            itemName.style.fontWeight = 'bold';
+            itemName.textContent = prato[i];
+            i++;
+
+            let itemPrice = document.createElement('div');
+            itemPrice.setAttribute('class', 'food-price');
+            itemPrice.textContent = prato[i];
+            i++;
+
+            let itemDesc = document.createElement('div');
+            itemDesc.textContent = prato[i];
+            
+
+            menu.appendChild(item);
+            item.appendChild(itemName);
+            item.appendChild(itemPrice);
+            item.appendChild(itemDesc);
+            if(i < (max-1)){
+                menu.appendChild(document.createElement('hr'));
+            }
+        }
+    })
 }
 
+function addStarter(){
+    starterButtonActivated();
 
+    let menu = document.getElementById('food-items');
+
+    while(menu.firstChild){
+        menu.removeChild(menu.lastChild);
+    }
+
+    var file = "starter.txt";
+
+    fetch(file)
+    .then(response => response.text())
+    .then(data =>{
+        var prato = data.split("\n");
+        var max = prato.length;
+
+        for(var i = 0; i <  max; i++){
+
+            let item = document.createElement('div');
+            item.setAttribute('class', 'food-item');
+
+            let itemName = document.createElement('div');
+            itemName.style.fontWeight = 'bold';
+            itemName.textContent = prato[i];
+            i++;
+
+            let itemPrice = document.createElement('div');
+            itemPrice.setAttribute('class', 'food-price');
+            itemPrice.textContent = prato[i];
+            i++;
+
+            let itemDesc = document.createElement('div');
+            itemDesc.textContent = prato[i];
+            
+
+            menu.appendChild(item);
+            item.appendChild(itemName);
+            item.appendChild(itemPrice);
+            item.appendChild(itemDesc);
+            if(i < (max-1)){
+                menu.appendChild(document.createElement('hr'));
+            }
+        }
+    })
+}
+
+/*
+function readTextFile(file)
+{
+    
+    fetch(file)
+    .then(response => response.text())
+    .then(data =>{
+        var prato = data.split("\n");
+        var max = prato.length;
+
+        for(var i = 0; i <  max; i++){
+            console.log(prato[i]);
+        }
+    })
+    
+}*/
 
 function pizzaButtonActivated(){
     document.getElementById('pizza-selector').classList.add('menu-type-selected');
